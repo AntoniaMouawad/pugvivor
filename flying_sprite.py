f285d89fecb3ef100d096a21a613f47bcfaaee83
Antonia Mouawad <antoniam@scribd.com>
2020-10-28 15:30:37 -0400

    Add Flying sprite that gets rid of out of screen sprites

---
+++ flying_sprite.py
@@ -0,0 +1,19 @@
+import arcade
+
+
+class FlyingSprite(arcade.Sprite):
+    """Base class for all flying sprites
+    Flying sprites include enemies and clouds
+    """
+
+    def update(self):
+        """Update the position of the sprite
+        When it moves off screen to the left, remove it
+        """
+
+        # Move the sprite
+        super().update()
+
+        # Remove if off the screen
+        if self.right < 0:
+            self.remove_from_sprite_lists()
